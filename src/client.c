bb02485f1cace32a952d5934f3344a5ac31ef596
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-11-02 21:17:46 +0530

    aof

--- src/client.c
+++ src/client.c
@@ -7,21 +7,25 @@
 #include <arpa/inet.h>
 #include "common.h"
 
+#define AOF_FILE "verokv.aof"
+#define AOF_ENABLED 0
+
+static void parse_resp(char *resp);
+void log_to_aof(FILE *aof, char *cmd);
+void replay_aof(int sfd, FILE *aof);
+
 int connect_server(char *addr, int port) {
     struct sockaddr_in serv_addr;
-    // create socket && verification
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1) {
         fputs("failed to create socket", stderr);
         exit(1);
     }
 
-    // assign ip & port
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(addr);
     serv_addr.sin_port = htons(port);
 
-    // connect
     if (connect(sfd, (SA *)&serv_addr, sizeof(serv_addr)) != 0) {
         fputs("failed to connect to server", stderr);
         exit(1);
@@ -41,7 +45,7 @@ static int parse_int(char *resp) {
     resp++;
     int n = 0, min = 0;
     if (*resp == '-') resp++, min++;
-    while (*resp != '\r') {
+    while (*resp != '\r' && *resp != '\0') {
         n = n * 10 + (*resp - '0');
         resp++;
     }
@@ -56,51 +60,134 @@ static char *parse_str(char *resp) {
     resp++;
 
     char *result = calloc(n + 1, sizeof(char));
-    for (int i = 0; i < n; i++) result[i] = resp[i];
-    result[n] = '\0';
-
+    if (result) {
+        strncpy(result, resp, n);
+        result[n] = '\0';
+    }
     return result;
 }
 
 static char *parse_err(char *resp) {
     resp++;
     char *result = malloc(strlen(resp) - 1);
-    for (int i = 0; i < strlen(resp) - 2; i++) result[i] = resp[i];
-    // result[strlen(result)] = '\0';
+    if (result) {
+        strncpy(result, resp, strlen(resp) - 2);
+        result[strlen(resp) - 2] = '\0';  // Null-terminate the string
+    }
     return result;
 }
 
 static void parse_resp(char *resp) {
     char *tmp;
-    switch(*resp) {
+    switch (*resp) {
         case '$':
-            tmp = parse_str(resp); 
-            tmp == NULL ? puts("(nil)") : print_quote_encase(tmp);
-            free(tmp);
+            tmp = parse_str(resp);
+            if (tmp == NULL) {
+                puts("(nil)");
+            } else {
+                print_quote_encase(tmp);
+                free(tmp);
+            }
             break;
         case ':':
             printf("(integer) %d\n", parse_int(resp));
             break;
-        case '*': break;
+        case '*': 
+            // Handle multi-bulk response if needed
+            break;
         case '-':
-            puts(parse_err(resp));
-        default: break;
+            tmp = parse_err(resp);
+            puts(tmp);
+            free(tmp);
+            break;
+        default:
+            break;
     }
 }
 
+void log_to_aof(FILE *aof, char *cmd) {
+    if (fputs(cmd, aof) == EOF) {
+        perror("Failed to write command to AOF file");
+        return;
+    }
+    fputc('\n', aof); 
+    if (fflush(aof) != 0) {
+        perror("Failed to flush AOF file");
+    }
+}
+
+void replay_aof(int sfd, FILE *aof) {
+    fseek(aof, 0, SEEK_SET);
+    char line[1024];
+    while (fgets(line, sizeof(line), aof) != NULL) {
+        writeline(sfd, line);
+        char *resp = readline(sfd);
+        parse_resp(resp);
+        free(resp);
+    }
+    printf("Data Regeneration Successful with AOF \n");
+}
+
 void repl(int sfd) {
+#if AOF_ENABLED
+    FILE *aof = fopen(AOF_FILE, "a+");
+    if (!aof) {
+        perror("Failed to open AOF file");
+        exit(1);
+    }
+
+    replay_aof(sfd, aof);
+#endif
+
     while (1) {
         char *inp = malloc(1024);
+        if (inp == NULL) {
+            perror("Failed to allocate memory");
+            break;
+        }
+        
         printf("verokv> ");
-
         fgets(inp, 1024, stdin);
-        writeline(sfd, inp);
 
+#if AOF_ENABLED
+        if (strncmp(inp, "set", 3) == 0 || 
+            strncmp(inp, "del", 3) == 0 || 
+            strncmp(inp, "hset", 4) == 0 || 
+            strncmp(inp, "hdel", 4) == 0 || 
+            strncmp(inp, "lpush", 5) == 0 || 
+            strncmp(inp, "rpush", 5) == 0 || 
+            strncmp(inp, "lpop", 4) == 0 || 
+            strncmp(inp, "rpop", 4) == 0 || 
+            strncmp(inp, "sadd", 4) == 0 || 
+            strncmp(inp, "srem", 4) == 0 || 
+            strncmp(inp, "hmset", 5) == 0 || 
+            strncmp(inp, "hmget", 5) == 0 || 
+            strncmp(inp, "incr", 4) == 0 || 
+            strncmp(inp, "decr", 4) == 0 || 
+            strncmp(inp, "incrby", 6) == 0 || 
+            strncmp(inp, "decrby", 6) == 0 || 
+            strncmp(inp, "append", 6) == 0 || 
+            strncmp(inp, "setnx", 6) == 0 || 
+            strncmp(inp, "mset", 4) == 0 || 
+            strncmp(inp, "msetnx", 6) == 0 || 
+            strncmp(inp, "hsetnx", 6) == 0 || 
+            strncmp(inp, "hincrby", 8) == 0 || 
+            strncmp(inp, "lrem", 4) == 0 || 
+            strncmp(inp, "lset", 4) == 0 || 
+            strncmp(inp, "ltrim", 6) == 0 || 
+            strncmp(inp, "rename", 6) == 0) {
+            log_to_aof(aof, inp);
+        }
+#endif
+
+        writeline(sfd, inp);
         char *resp = readline(sfd);
-        // printf("%d\n", resp == NULL);
         if (resp == NULL) break;
         parse_resp(resp);
         free(inp);
     }
-}
 
+#if AOF_ENABLED
+    fclose(aof);
+#endif
+}
